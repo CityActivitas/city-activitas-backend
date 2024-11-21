18f1a5740c837a80f7c31ee7ab9a040a65dac155
Wei Lee <weilee.rx@gmail.com>
2024-11-21 23:18:30 +0800

    test: add minimum test cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,16 @@ import pytest
 from fastapi.testclient import TestClient
 
 
+async def _mock_verify_token():
+    return {}
+
+
 @pytest.fixture
 @mock.patch.dict(os.environ, {"SUPABASE_URL": "url"})
 @mock.patch.dict(os.environ, {"SUPABASE_ANON_KEY": "key"})
+@mock.patch("server.dependencies.auth.get_auth_dependency", return_value=_mock_verify_token)
 @mock.patch("supabase.create_client")
-def test_client(create_client):
+def test_client(supabase_client, get_auth):
     from server.main import app
 
     return TestClient(app)
